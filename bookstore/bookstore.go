cb69f763e105adee757415b6bbbafe28247a89a8
Luqman <luqmanhafizimohdzawawi@gmail.com>
2023-10-30 00:00:59 +0800

    feat: building the app

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,1 +1,11 @@
 package bookstore
+
+type Customer struct {
+	name  string
+	email string
+}
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+}
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -1,1 +1,14 @@
-package bookstore
+package bookstore_test
+
+import (
+	"happy-fun-book/bookstore"
+	"testing"
+)
+
+func TestBook(t *testing.T) {
+	t.Parallel()
+	_ = bookstore.Book{
+		Title:  "Hang Tuai",
+		Author: "M Nusir",
+		Copies: 10}
+}
